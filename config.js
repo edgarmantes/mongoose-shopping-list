fadc888608428db968513d9afa1b2bf5f2404687
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-08 16:53:23 -0600

    made corrections for config.js and package.json

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.DATABASE_URL = 172.25.0.0/16 ||
+exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
