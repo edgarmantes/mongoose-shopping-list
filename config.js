2317eb9c3d5078d9636d7b074c06669c74e8338c
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-08 18:05:20 -0600

    Pointed production config to mLab, testing

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+exports.DATABASE_URL = process.env.DATABASE_URL || mlab-url-here
+
+// mongoose.connect(process.env.DB_URL || mlab-url-here)
+//                        global.DATABASE_URL ||
+//                        (process.env.NODE_ENV === 'production' ?
+//                             'mongodb://localhost/shopping-list' :
+//                             'mongodb://localhost/shopping-list-dev':
+//                             )
 exports.PORT = process.env.PORT || 8080;
 
