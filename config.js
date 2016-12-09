a35c8fcd3d8a517329e9fb4a6b3e785735af83f4
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-08 20:48:14 -0600

    removed simicolon from config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL =   process.env.DATABASE_URL || global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev':
+                            'mongodb://localhost/shopping-list-dev'
                             )
 
 
