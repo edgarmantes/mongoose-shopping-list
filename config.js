0c52d07274a841f9517f0cad2b40d8721ddd06a9
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-08 18:16:57 -0600

    Attempting to use Travis CI deployment

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || mlab-url-here
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://<dbuser>:<dbpassword>@ds127928.mlab.com:27928/mongodb-shopping-list-thinkful'
 
 // mongoose.connect(process.env.DB_URL || mlab-url-here)
 //                        global.DATABASE_URL ||
