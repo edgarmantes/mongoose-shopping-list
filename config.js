f3871e30e8a43aa20d7f43c1701c6374bf3821d5
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-08 21:08:51 -0600

    testing mLab connection

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.DATABASE_URL =   process.env.DATABASE_URL || global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
+                       		'mongodb://<dbuser>:<dbpassword>@ds127928.mlab.com:27928/mongodb-shopping-list-thinkful':
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev':
-                            'mongodb://<dbuser>:<dbpassword>@ds127928.mlab.com:27928/mongodb-shopping-list-thinkful'
+                            'mongodb://localhost/shopping-list-dev'
                             )
 
 
