0d16545995d738814eeb6416c97782bed05d02de
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-09 11:09:55 -0600

    testing for deployment

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.DATABASE_URL =   process.env.DATABASE_URL || global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                       		'mongodb://emantes:Lakers7!ds127928.mlab.com:27928/mongodb-shopping-list-thinkful':
-                            'mongodb://localhost/shopping-list':
-                            'mongodb://localhost/shopping-list-dev')
+                       		'mongodb://emantes:Lakers7!ds127928.mlab.com:27928/mongodb-shopping-list-thinkful')
+                            // 'mongodb://localhost/shopping-list':
+                            // 'mongodb://localhost/shopping-list-dev')
                             
 
 
