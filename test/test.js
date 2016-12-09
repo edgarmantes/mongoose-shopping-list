e00217b866b614cefdafcc52c82c2f82ce12dec2
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-09 08:45:05 -0600

    testing connection for test.js file

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV === 'production' ?
-                            'mongodb://ds127928.mlab.com:27928/mongodb-shopping-list-thinkful':
+                            'mongodb://ds127928.mlab.com:27928/mongodb-shopping-list-thinkful'
 
 // global.DATABASE_URL = 'mongodb://localhost/shopping-list-test';
 
