e8d09f15e6f50cfbf10688a1b81be7717d4beaee
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-09 12:33:03 -0600

    reset code to last working code

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 exports.DATABASE_URL =   process.env.DATABASE_URL || global.DATABASE_URL ||
-                       process.env.NODE_ENV === 'production' ?
-                       		'mongodb://emantes:Lakers7!ds127928.mlab.com:27928/mongodb-shopping-list-thinkful'
-                            // 'mongodb://localhost/shopping-list'
-                            // 'mongodb://localhost/shopping-list-dev')
+                       (process.env.NODE_ENV === 'production' ?
+                       		// 'mongodb://emantes:Lakers7!ds127928.mlab.com:27928/mongodb-shopping-list-thinkful'
+                            'mongodb://localhost/shopping-list'
+                            'mongodb://localhost/shopping-list-dev')
                             
 
 
-// "mongodb://<dbuser>:<dbpassword>@ds127928.mlab.com:27928/mongodb-shopping-list-thinkful"
 exports.PORT = process.env.PORT || 8080;
 
