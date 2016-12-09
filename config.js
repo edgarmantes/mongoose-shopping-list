396fe8aea7788f6a004f9cba518155868987d00f
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-08 20:45:23 -0600

    Testing last working code

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-exports.DATABASE_URL = "mongodb://<dbuser>:<dbpassword>@ds127928.mlab.com:27928/mongodb-shopping-list-thinkful"
+exports.DATABASE_URL =   process.env.DATABASE_URL || global.DATABASE_URL ||
+                       (process.env.NODE_ENV === 'production' ?
+                            'mongodb://localhost/shopping-list' :
+                            'mongodb://localhost/shopping-list-dev':
+                            )
 
-//   process.env.DATABASE_URL || global.DATABASE_URL ||
-//                        (process.env.NODE_ENV === 'production' ?
-//                             'mongodb://localhost/shopping-list' :
-//                             'mongodb://localhost/shopping-list-dev':
-//                             )
+
+// "mongodb://<dbuser>:<dbpassword>@ds127928.mlab.com:27928/mongodb-shopping-list-thinkful"
 exports.PORT = process.env.PORT || 8080;
 
