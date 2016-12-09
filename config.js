eb953a22e471e822c8561186e34df1fe05f9bc31
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-08 20:40:49 -0600

    Fixing mLab connection

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://<dbuser>:<dbpassword>@ds127928.mlab.com:27928/mongodb-shopping-list-thinkful'
+exports.DATABASE_URL = "mongodb://<dbuser>:<dbpassword>@ds127928.mlab.com:27928/mongodb-shopping-list-thinkful"
 
-// mongoose.connect(process.env.DB_URL || mlab-url-here)
-//                        global.DATABASE_URL ||
+//   process.env.DATABASE_URL || global.DATABASE_URL ||
 //                        (process.env.NODE_ENV === 'production' ?
 //                             'mongodb://localhost/shopping-list' :
 //                             'mongodb://localhost/shopping-list-dev':
